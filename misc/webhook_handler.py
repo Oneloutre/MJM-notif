852a8c30859f07ae1c45328b5ae950c86901bcf1
Onelots <54822802+Oneloutre@users.noreply.github.com>
2024-01-06 02:25:37 +0100

    :zap: optimized script • fixed and started scraping • managing scraping • managing infos • others fixes

--- misc/webhook_handler.py
+++ misc/webhook_handler.py
@@ -17,15 +17,36 @@ def webhook_check():
         exit(1)
 
 
-def webhook_send(webhook, message):
+def webhook_send(webhook, url_cours, title, color, start, end):
     url = webhook
     data = {
         "embeds": [
-            {
-                "title": "Connexion réussie!",
-                "description": message,
-                "color": 65280
-            }
-        ]
+        {
+            "type": "rich",
+            "title": "Des devoirs ont été ajoutés !",
+            "description": title,
+            "color": int(color.replace("#", ""), 16),
+            "thumbnail": {
+                "url": "https://storage.letudiant.fr/osp/cards/276/mjm-graphic-design-231102102947.jpg",
+                "height": 0,
+                "width": 0,
+                },
+            "author": {
+                "name": title,
+                "url": url_cours,
+                },
+                "icon_url": "https://cdn.discordapp.com/guilds/749473022821400596/users/70569212453191698/avatars/1b3e0342e581ad0313ba94cf6043494a.webp",
+            "footer": {
+                "text": "Pour le " + start,
+                    },
+            }]
     }
-    requests.post(url, json=data)
+
+    result = requests.post(url, json=data)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
