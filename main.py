ce1c5a851444cfa76310f50995068e216b6efa10
Onelots <54822802+Oneloutre@users.noreply.github.com>
2024-01-06 17:20:05 +0100

    :ambulance: - added log_webhook to keep an eye on the container !!!

--- main.py
+++ main.py
@@ -10,22 +10,26 @@ import functools
 
 
 cookies_file = 'files/session_cookies.pkl'
+webhook = webhook_handler.webhook_check()
+log_webhook = webhook_handler.log_webhook_check()
 
 
 def main():
 
     username, password = connection.creds_check()
     response, session = connection.connection(username, password)
-    webhook = webhook_handler.webhook_check()
+
+
 
     if 'Ma carte d\'étudiant' in response.text:
             print('Connexion réussie!')
-            #webhook_handler.webhook_send(webhook, 'Connexion réussie!')
+            if os.path.exists('files/log_webhook.txt'):
+                webhook_handler.simple_send(log_webhook, 'Connexion réussie!')
             json_parser.parser(session, webhook)
             with open(cookies_file, 'wb') as f:
                 pickle.dump(session.cookies, f)
             scheduler = schedule.Scheduler()
-            scheduler.every(15).minutes.do(functools.partial(update_checker.comparer_devoirs_json, session))
+            scheduler.every(15).minutes.do(functools.partial(update_checker.comparer_devoirs_json, session, log_webhook))
 
             while True:
                 scheduler.run_pending()
@@ -36,6 +40,8 @@ def main():
             main()
         else:
             print('Erreur: Connexion échouée!')
+            if os.path.exists('files/log_webhook.txt'):
+                webhook_handler.simple_send(log_webhook, 'Erreur: Connexion échouée!')
             os.remove("files/creds.txt")
             os.remove("files/webhook.txt")
             main()
@@ -47,5 +53,6 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         print('Erreur: ' + str(e))
-
+        if os.path.exists('files/log_webhook.txt'):
+            webhook_handler.simple_send(log_webhook, 'Erreur: ' + str(e))
         main()
